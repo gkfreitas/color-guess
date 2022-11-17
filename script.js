e0b5b6f6a397bf32cfe211818c25cbca49012581
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-17 13:48:42 -0300

    REQ 5 - Finalizado, resposta a cor clicada adicionada

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Criar cores aleatorias rgb
+// Criar cores aleatorias rgb.
 
 function randomColor() {
   const randomNumber1 = Math.floor(Math.random() * 256);
@@ -7,10 +7,26 @@ function randomColor() {
   return `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
 }
 
-console.log(randomColor());
-
 const balls = document.getElementById('ball-container').children;
+const answerText = document.getElementById('answer');
+const rgbColor = document.getElementById('rgb-color');
+
+// Mostrar se a cor escolhida está certa.
+
+function showAnswer(event) {
+  const eventTarget = event.target;
+  const colorEventTarget = eventTarget.style.backgroundColor;
+  if (colorEventTarget === rgbColor.innerText) {
+    answerText.innerText = 'Acertou!';
+  } else {
+    answerText.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+// Adicionar cores aleatoria as bolas e evento quando clicar nas bolas.
 
 for (let i = 0; i < balls.length; i += 1) {
+  balls[0].style.backgroundColor = 'rgb(168, 34, 1)';
   balls[i].style.backgroundColor = randomColor();
+  balls[i].addEventListener('click', showAnswer);
 }
